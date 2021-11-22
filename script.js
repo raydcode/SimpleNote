31eb28adf393aabd50a0e355e9931da4584a81e5
Ray <qc@pop-os.localdomain>
2021-11-22 20:47:39 +0530

    added:comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const addBtn  = document.getElementById('add');
 
 const notes = JSON.parse(localStorage.getItem('notes'));  
 
-
+//Update Local Storage
 
 const updateNotes = ()=>{
     const notesText = document.querySelectorAll('textarea');
